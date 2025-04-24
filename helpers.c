275f39b7020b3a8e7ecea968a46297ebb9883c27
GG-Muniz <g.garciamuniz@outlook.com>
2025-04-24 14:01:02 -0400

    Eliminating redundancy in the helper function file - it gave an error in compilation.

--- helpers.c
+++ helpers.c
@@ -19,39 +19,3 @@ int is_only_whitespace(char *str)
 	}
 	return (1);
 }
-/**
- * display_prompt - displays shell prompt.
- */
-void display_prompt(void)
-{
-	if (isatty(STDIN_FILENO))
-		printf("#cisfun$ ");
-}
-/**
- * read_command_line - function will read a line from the stdin.
- * Return: pointer to input string, or NULL on EOF error.
- *
- */
-char *read_command_line(void)
-{
-	char *line = NULL;
-	size_t buffer_size = 0;
-	ssize_t chars_read;
-
-	/* Read line from stdin. */
-	chars_read = getline(&line, &buffer_size, stdin);
-
-	/* Check for EOF or error. */
-	if (chars_read == -1)
-	{
-		/* Free memory if getline fails. */
-		free(line);
-		return (NULL);
-	}
-
-	/* Remove the trailing newline character */
-	if (line[chars_read - 1] == '\n')
-		line[chars_read - 1] = '\0';
-
-	return (line);
-}
