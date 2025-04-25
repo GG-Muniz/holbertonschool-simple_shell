baa744531e3967e3efa67f8bedf908754a05272f
AngelD89 <angeldeprey@yahoo.com>
2025-04-25 11:16:27 -0400

    execve

--- execve.c
+++ execve.c
@@ -1,41 +1,48 @@
 #include "shell.h"
+
 /**
- * execute_command - Execute a command with its arguments
- * @args: Array of strings containing the command and its arguments
- * @program_name: Name of the shell program for error messages
+ * execute_command - Execute a command
+ * @args: Command and arguments
  *
- * Return: 1 to continue the shell, 0 to exit
+ * Return: 1 to continue, 0 to exit shell
  */
 int execute_command(char **args)
 {
 	pid_t pid;
 	int status;
+	char *command_path;
 
-	/* Fork a child process */
-	pid = fork();
+	if (!args[0])
+		return (1);
 
-	if (pid == -1)
+	if (strcmp(args[0], "exit") == 0)
+		return (0);
+
+	command_path = find_command(args[0]);
+	if (!command_path)
 	{
-		/* Fork error */
-		perror("Error forking process");
-		return 1;
+		fprintf(stderr, "%s: command not found\n", args[0]);
+		return (1);
 	}
 
+	pid = fork();
 	if (pid == 0)
 	{
-		/* Child process - attempt to execute the command */
-		if (execve(args[0], args, environ) == -1)
+		if (execv(command_path, args) == -1)
 		{
-			/* Format error message to match expected output */
-			fprintf(stderr, "%s: No such file or directory\n", args[0]);
+			perror("excv");
 			exit(EXIT_FAILURE);
 		}
 	}
+	else if (pid < 0)
+	{
+		perror("fork");
+	}
 	else
 	{
-		/* Parent process - wait for child to complete */
 		waitpid(pid, &status, 0);
 	}
 
-	return 1;
+	free(command_path);
+	return (1);
 }
